6d01a6c42f898ab8ae563bd0ea780d394358400e
Tal Zwikael <33423@cgs.act.edu.au>
2015-07-30 15:06:16 +1000

    Added second view controller

---
+++ TestProject/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  TestProject
+//
+//  Created by Tal Zwikael on 30/07/2015.
+//  Copyright (c) 2015 cgscomputing. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		
+		super.viewDidLoad()
+		
+		println("Hello from the second view controller")
+		
+	}
+	
+	override func didReceiveMemoryWarning() {
+		
+		super.didReceiveMemoryWarning()
+		
+		// Dispose of any resources that can be recreated.
+		
+	}
+	
+}
